a1d38445e351b15dc6118624939a80bbc480fbc6
Mehul <mehultandale@gmail.com>
2022-12-12 15:46:10 +0530

    > Removing the scaffolding code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
-import logo from './logo.svg';
+
 import './App.css';
 
 
 function App() {
     return (
         <div>
+            <h3>Upcoming classes</h3>
+            <ul>
+                <li>
+                    <h5>Optimism, Pessimism and Realism</h5>
+                    <p>Understand how these terms reconcile with each other.</p>
+                </li>
+            </ul>
         </div>
     );
 }
